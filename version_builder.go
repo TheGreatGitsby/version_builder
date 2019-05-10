a208bc858651a99972ea81bd10b86c5a09eeb4a2
TheGreatGitsby <nicholas.p.stroupe@gmail.com>
2019-05-09 20:00:57 -0400

    im a commit

--- version_builder.go
+++ version_builder.go
@@ -17,10 +17,16 @@ func BootstrapPage(w http.ResponseWriter, r *http.Request) {
    fmt.Println("served bootstrap page")
 }
 
+func githubHandler(w http.ResponseWriter, r *http.Request) {
+   fmt.Println("got the github hook!")
+}
+
 func main() {
    http.HandleFunc("/", homePage) // set router
    http.HandleFunc("/bootstrap", BootstrapPage) // set router
    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+   http.HandleFunc("/gitwebhook", githubHandler) // set router
+
    err := http.ListenAndServe(":8090", nil) // set listen port
    if err != nil {
       log.Fatal("ListenAndServe: ", err)
