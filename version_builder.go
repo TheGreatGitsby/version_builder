d56fd366d1ed3b5d507b33deec8851c9b32288d0
TheGreatGitsby <nicholas.p.stroupe@gmail.com>
2019-05-09 20:18:49 -0400

    added git webhook

--- version_builder.go
+++ version_builder.go
@@ -5,6 +5,7 @@ import (
    "net/http"
    //"net/http/httputil"
    "log"
+   "gopkg.in/rjz/githubhook.v0"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
@@ -19,6 +20,12 @@ func BootstrapPage(w http.ResponseWriter, r *http.Request) {
 
 func githubHandler(w http.ResponseWriter, r *http.Request) {
    fmt.Println("got the github hook!")
+   secret := []byte("imasecret")
+   hook, err := githubhook.Parse(secret, r)
+   fmt.Println("heres the hook payload", hook.Payload)
+   if err != nil {
+      log.Fatal("fatal parsing hook", err)
+   }
 }
 
 func main() {
@@ -27,7 +34,7 @@ func main() {
    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
    http.HandleFunc("/gitwebhook", githubHandler) // set router
 
-   err := http.ListenAndServe(":8090", nil) // set listen port
+   err := http.ListenAndServe(":80", nil) // set listen port
    if err != nil {
       log.Fatal("ListenAndServe: ", err)
    }
